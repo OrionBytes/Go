0b72f3ee33111502644b7966e87ab78cf233aa43
orion.bytes@gmail.com <orion.bytes@gmail.com>
2019-10-19 02:10:13 +0200

    what the fuck is with vue?

---
+++ add_users.py
@@ -0,0 +1,5 @@
+from admintools import make_user
+
+#make_user(name='admin', password='admin',email='admin@admin.com',rank=100)
+make_user(name='admin223', password='admin',email='admin222@admin.com',rank=100)
+make_user(name='admin333', password='admin',email='admin333@admin.com',rank=100)
